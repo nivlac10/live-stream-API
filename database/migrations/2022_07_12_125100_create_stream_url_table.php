96d58164dbe48f9a474e6643738a1cb580e6f62a
nivlac10 <45712558+nivlac10@users.noreply.github.com>
2022-07-26 14:26:58 +0200

    Update 2022_07_12_125100_create_stream_url_table.php
    change of migration seed

--- database/migrations/2022_07_12_125100_create_stream_url_table.php
+++ database/migrations/2022_07_12_125100_create_stream_url_table.php
@@ -15,14 +15,7 @@ return new class extends Migration
     {
         Schema::create('stream_url', function (Blueprint $table) {
             $table->id();
-            $table->time('time');
-            $table->string('sports_type');
-            $table->string('home_team');
-            $table->string('home_mark');
-            $table->string('away_team');
-            $table->string('away_mark');
-            $table->date('start_date');
-            $table->string('status');
+            $table->string('url',2048);
             $table->string('uid',2048);
             $table->timestamps();
         });
